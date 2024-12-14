0f6221c66c773164fa9e89732bd463793258829e
Callmegod <125900081+Callmeg0d@users.noreply.github.com>
2024-12-14 13:21:02 +0300

    Добавлено обновление оценки, уделение оценок

--- main/ui/profile.py
+++ main/ui/profile.py
@@ -21,7 +21,7 @@ class StudentProfile(QtWidgets.QWidget):
         super().__init__()
 
         self.setWindowTitle("Профиль студента")
-        self.setGeometry(500, 200, 1000, 700)
+        self.setGeometry(470, 200, 1000, 700)
 
         layout = QtWidgets.QVBoxLayout()
 
@@ -49,24 +49,32 @@ class StudentProfile(QtWidgets.QWidget):
         # Создаем таблицу для отображения оценок
         self.table_widget = QtWidgets.QTableWidget()
 
+        group_name, average_grade, subjects_info = self.get_student_info(student_id)
+
         num_rows = len(subjects_info)
-        self.table_widget.setRowCount(num_rows)
+        self.table_widget.setRowCount(num_rows if num_rows > 0 else 1)  # Устанавливаем 1 строку, если нет оценок
         self.table_widget.setColumnCount(3)
 
+        # Устанавливаем заголовки таблицы
         self.table_widget.setHorizontalHeaderLabels(["Предмет", "Оценка", "Преподаватель"])
 
-        for row_index, (subject_name, grade, teacher_full_name) in enumerate(subjects_info):
-            subject_item = QtWidgets.QTableWidgetItem(subject_name)
-            subject_item.setTextAlignment(QtCore.Qt.AlignCenter)
-            self.table_widget.setItem(row_index, 0, subject_item)
+        if subjects_info:
+            for row_index, (subject_name, grade, teacher_full_name) in enumerate(subjects_info):
+                subject_item = QtWidgets.QTableWidgetItem(subject_name)
+                subject_item.setTextAlignment(QtCore.Qt.AlignCenter)
+                self.table_widget.setItem(row_index, 0, subject_item)
 
-            grade_item = QtWidgets.QTableWidgetItem(str(grade))
-            grade_item.setTextAlignment(QtCore.Qt.AlignCenter)
-            self.table_widget.setItem(row_index, 1, grade_item)
+                grade_item = QtWidgets.QTableWidgetItem(str(grade))
+                grade_item.setTextAlignment(QtCore.Qt.AlignCenter)
+                self.table_widget.setItem(row_index, 1, grade_item)
 
-            teacher_item = QtWidgets.QTableWidgetItem(teacher_full_name)
-            teacher_item.setTextAlignment(QtCore.Qt.AlignCenter)
-            self.table_widget.setItem(row_index, 2, teacher_item)
+                teacher_item = QtWidgets.QTableWidgetItem(teacher_full_name)
+                teacher_item.setTextAlignment(QtCore.Qt.AlignCenter)
+                self.table_widget.setItem(row_index, 2, teacher_item)
+        else:  # Если нет оценок, оставляем первую строку пустой
+            self.table_widget.setItem(0, 0, QtWidgets.QTableWidgetItem(""))
+            self.table_widget.setItem(0, 1, QtWidgets.QTableWidgetItem(""))
+            self.table_widget.setItem(0, 2, QtWidgets.QTableWidgetItem(""))
 
         self.table_widget.setColumnWidth(0, 300)
         self.table_widget.setColumnWidth(1, 100)
@@ -80,10 +88,6 @@ class StudentProfile(QtWidgets.QWidget):
 
         # Запрет редактирования ячеек
         self.table_widget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
-
-        # Устанавливаем фиксированную высоту таблицы
-        row_height = self.table_widget.rowHeight(0)  # Получаем высоту первой строки
-        self.table_widget.setFixedHeight(row_height * min(num_rows, 10))
 
         central_layout.addWidget(self.table_widget)
 
@@ -117,12 +121,19 @@ class StudentProfile(QtWidgets.QWidget):
                 conn.close()
 
         if results:
-            group_name = results[0][0]
-            average_grade = results[0][1]
-            subjects_info = [(row[2], row[3], f"{row[4]} {row[5]} {row[6]}") for row in results]
+            group_name = results[0][0] if results[0][0] is not None else "Неизвестная группа"
+            average_grade = results[0][1] if results[0][1] is not None else 0.0
+
+            # Проверяем, содержатся ли оценки
+            subjects_info = []
+            for row in results:
+                if row[2] is not None:
+                    grade = row[3] if row[3] is not None else "Неизвестная"
+                    teacher_full_name = f"{row[4] or ''} {row[5] or ''} {row[6] or ''}".strip()
+                    subjects_info.append((row[2], grade, teacher_full_name))
             return group_name, average_grade, subjects_info
         else:
-            return ("Неизвестная группа", 0.0, [])
+            return "Неизвестная группа", 0.0, []
 
 
 class TeacherProfile(QtWidgets.QWidget):
@@ -134,7 +145,7 @@ class TeacherProfile(QtWidgets.QWidget):
         super().__init__()
 
         self.setWindowTitle("Профиль преподавателя")
-        self.setGeometry(500, 200, 1000, 700)
+        self.setGeometry(470, 200, 1000, 700)
 
         layout = QtWidgets.QVBoxLayout()
 
@@ -143,6 +154,8 @@ class TeacherProfile(QtWidgets.QWidget):
         self.name_label = QtWidgets.QLabel(f"Пользователь: {first_name} {last_name} {middle_name}")
         self.name_label.setAlignment(QtCore.Qt.AlignCenter)
         layout.addWidget(self.name_label)
+
+        layout.addStretch(1)
 
         self.grade_button = QtWidgets.QPushButton("Выставить оценку", self)
         self.grade_button.clicked.connect(lambda: self.open_grade_form(teacher_id))
@@ -168,15 +181,38 @@ class TeacherProfile(QtWidgets.QWidget):
         self.grades_info = self.get_grades_for_teacher(teacher_id)  # Получаем оценки для преподавателя
         self.insert_data_in_table(self.grades_info)  # Заполняем таблицу начальными данными
 
-        layout.addWidget(self.table_widget)
-
+        layout.addStretch(1)
         self.save_button = QtWidgets.QPushButton("Обновить оценки", self)
         self.save_button.clicked.connect(self.save_grades)
         layout.addWidget(self.save_button)
 
+        layout.addWidget(self.table_widget)
+
+        self.name_label = QtWidgets.QLabel("Для удаление записи выберите её, кликнув на её номер в первом столбце, и нажмите кнопку ниже")
+        self.name_label.setAlignment(QtCore.Qt.AlignCenter)
+        layout.addWidget(self.name_label)
+
+        # Установка режима выбора для таблицы
+        self.table_widget.setSelectionMode(QtWidgets.QAbstractItemView.MultiSelection)
+
+        self.delete_button = QtWidgets.QPushButton("Удалить выбранные строки")
+        self.delete_button.clicked.connect(self.delete_selected_rows)
+        layout.addWidget(self.delete_button)
+
         layout.addStretch(6)
 
         self.setLayout(layout)
+
+    def delete_selected_rows(self):
+        selected_rows = sorted(
+            set(index.row() for index in self.table_widget.selectedIndexes()))
+        if selected_rows:
+            for row in reversed(selected_rows):
+                self.table_widget.removeRow(row)
+                print(f"Строка {row} удалена.")
+        else:
+            QtWidgets.QMessageBox.warning(self, "Ошибка", "Выберите строки для удаления.")
+
 
     def insert_data_in_table(self, grades_info):
         """Заполняет таблицу данными оценок."""
@@ -313,7 +349,7 @@ class GradeForm(QtWidgets.QWidget):
         super().__init__()
         self.teacher_id = teacher_id  # ID преподавателя, который авторизован
         self.setWindowTitle("Выставление оценки")
-        self.setGeometry(750, 400, 500, 350)
+        self.setGeometry(675, 300, 600, 450)
         layout = QtWidgets.QVBoxLayout()
 
         self.first_name_input = QtWidgets.QLineEdit(self)
@@ -350,39 +386,41 @@ class GradeForm(QtWidgets.QWidget):
         grade = self.grade_input.text()
 
         try:
-            conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD,
-                                    host=DB_HOST)
+            conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
             cursor = conn.cursor()
 
             # Получаем id студента
-            cursor.execute(
-                "SELECT * FROM get_student_id(%s, %s, %s)",
-                (first_name, last_name, group_name))
+            cursor.execute("SELECT * FROM get_student_id(%s, %s, %s)", (first_name, last_name, group_name))
             student_id_result = cursor.fetchone()
+
             if student_id_result:
                 student_id = student_id_result[0]
 
                 # Проверяем наличие предмета
-                cursor.execute("SELECT * FROM get_subject_id(%s, %s)",
-                               (subject_name, self.teacher_id))
+                cursor.execute("SELECT * FROM get_subject_id(%s, %s)", (subject_name, self.teacher_id))
                 subject_id_result = cursor.fetchone()
 
                 if subject_id_result and subject_id_result[0] is not None:
                     subject_id = subject_id_result[0]
                 else:
-                    cursor.execute("SELECT insert_subject(%s, %s)",
-                                   (subject_name, self.teacher_id))
+                    cursor.execute("SELECT insert_subject(%s, %s)", (subject_name, self.teacher_id))
                     subject_id = cursor.fetchone()[0]
                     conn.commit()
 
-                cursor.execute("SELECT insert_grade(%s, %s, %s, %s)",
-                               (student_id, subject_id, self.teacher_id, grade))
-                conn.commit()
-                QtWidgets.QMessageBox.information(self, "Успех", "Оценка успешно выставлена!")
-                if cursor:
-                    cursor.close()
-                if conn:
-                    conn.close()
+                # Проверяем, существует ли оценка для данного студента и предмета
+                cursor.execute("SELECT * FROM grades WHERE student_id = %s AND subject_id = %s",
+                               (student_id, subject_id))
+                existing_grade = cursor.fetchone()
+
+                if existing_grade:
+                    QtWidgets.QMessageBox.warning(self, "Ошибка", 'Оценка за предмет уже выставлена. Вы можете её изменить.')
+                else:
+                    # Вставляем новую оценку
+                    cursor.execute("SELECT insert_grade(%s, %s, %s, %s)",
+                                   (student_id, subject_id, self.teacher_id, grade))
+                    conn.commit()
+                    QtWidgets.QMessageBox.information(self, "Успех", "Оценка успешно выставлена!")
+
             else:
                 QtWidgets.QMessageBox.warning(self, "Ошибка", "Студент не найден.")
         except Exception as e:
@@ -392,4 +430,3 @@ class GradeForm(QtWidgets.QWidget):
                 cursor.close()
             if conn:
                 conn.close()
-
